9429b9ee239ff437b5a595d73dead330c4b21b41
chethandeshpande <chethandec22@gmail.com>
2016-08-24 15:41:36 +0530

    Chethan, Yugesh | Changed the order of sections in registration create new page. Copying values of caretaker section to legal representative section if caretaker same as legal representative.

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -35,5 +35,33 @@ Bahmni.Registration.AttributesConditions.rules = {
         }
         return returnValues
 
+    },
+    'caretakeralsoLegalRep': function(patient) {
+        var returnValues = {
+            show: [],
+            hide: []
+        };
+        if (patient["caretakeralsoLegalRep"] && patient["caretakeralsoLegalRep"].value && patient["caretakeralsoLegalRep"].value.fullySpecifiedName === "Yes") {
+            returnValues.show.push("LegalRepresentative");
+            patient["legalRepFullNameEnglish"] = patient["caretakerNameEnglish"];
+            patient["legalRepFullNameArabic"] = patient["caretakerNameArabic"];
+            patient["legalRepGender"] = patient["caretakerGender"];
+            patient["legalRepDob"] = patient["caretakerDob"];
+            patient["legalRepNationality"] = patient["caretakerNationality"]
+        } else if((patient["age"].years === undefined || patient["age"].years > 18) && patient["caretakeralsoLegalRep"] && (patient["caretakeralsoLegalRep"].value === undefined || patient["caretakeralsoLegalRep"].value.fullySpecifiedName === "No")){
+            returnValues.hide.push("LegalRepresentative");
+            patient["legalRepFullNameEnglish"] = undefined;
+            patient["legalRepFullNameArabic"] = undefined;
+            patient["legalRepGender"] = undefined;
+            patient["legalRepDob"] = undefined;
+            patient["legalRepNationality"] = undefined
+        } else {
+            patient["legalRepFullNameEnglish"] = undefined;
+            patient["legalRepFullNameArabic"] = undefined;
+            patient["legalRepGender"] = undefined;
+            patient["legalRepDob"] = undefined;
+            patient["legalRepNationality"] = undefined
+        }
+        return returnValues
     }
 };
