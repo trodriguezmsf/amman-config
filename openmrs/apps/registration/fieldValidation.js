bf9629853384826f1ea5deb211ff65dece1858d3
chethandeshpande <chethandec22@gmail.com>
2016-09-15 20:06:50 +0530

    Chethan, Shiva | Validations for date fields.

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -1,20 +1,23 @@
 Bahmni.Registration.customValidator = {
-    "age.days": {
-        method: function (name, value) {
-            return value >= 0;
-        },
-        errorMessage: "REGISTRATION_AGE_ERROR_KEY"
-    },
-    "Telephone Number": {
+    "legalRepDob": {
         method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
+            var dateUtil = Bahmni.Common.Util.DateUtil;
+            return moment(value).isBefore(dateUtil.today());
         },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
+        "errorMessage": "REGISTRATION_LGDOB_ERROR_KEY"
     },
-    "caste": {
+    "caretakerDob": {
         method: function (name, value, personAttributeDetails) {
-            return value.match(/^\w+$/);
+            var dateUtil = Bahmni.Common.Util.DateUtil;
+            return moment(value).isBefore(dateUtil.today());
         },
-        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
+        "errorMessage": "REGISTRATION_CTDOB_ERROR_KEY"
     },
+    "id3DateOfExpiry": {
+        method: function (name, value, personAttributeDetails) {
+            var dateUtil = Bahmni.Common.Util.DateUtil;
+            return moment(value).isAfter(dateUtil.today());
+        },
+        "errorMessage": "REGISTRATION_DOE_ERROR_KEY"
+    }
 };
