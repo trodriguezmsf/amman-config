947b5606ec068eb4953bf3ca92beeb30c6deb71d
yugesh vohra <yug.vohra@gmail.com>
2016-09-20 15:37:24 +0530

    # 77 |remote server address and directories should be sent as command line arguments

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-REMOTE_SERV_ADDRESS='vohray@10.136.20.21';
-REMOTE_SERV_SYNC_DIR='/Users/vohray/yug_ws/temp';
+#REMOTE_SERV_ADDRESS='vohray@10.136.20.21';
+#REMOTE_SERV_SYNC_DIR='/Users/vohray/yug_ws/temp';
 MAILING_ADDRESS_ON_FAILURE='vyugesh@thoughtworks.com'
 DIR_TO_BACKUP='/db-backup/,/home/bahmni/uploaded_results,/home/bahmni/document_images,/home/bahmni/.bahmni-reports,/home/bahmni/patient_images,/home/bahmni/uploaded-files'
 SENDMAIL=false
-while getopts "s:m:d:f:" opt ;
+
+#parsing the command line options
+while getopts "m:d:f:" opt ;
 do
     case $opt in
-        s) REMOTE_SERV_ADDRESS="$OPTARG";;
         m) SENDMAIL="$OPTARG";;
         d) DIR_TO_BACKUP="$OPTARG";;
         f) MAILING_ADDRESS_ON_FAILURE="$OPTARG";;
@@ -15,13 +16,26 @@ do
         exit 0 ;;
     esac
 done
+
+#fetching the command line arguments for remote server address and directory
+while [[ "$1" == -* ]];
+do
+    shift 2;
+done
+
+REMOTE_SERV_ADDRESS="$1";
+REMOTE_SERV_SYNC_DIR="$2";
+
 DIRS=(${DIR_TO_BACKUP//,/ })
+
 STATUS_MESSAGE_PREFIX="Directories"
 STATUS_MESSAGE="";
 STATUS_MESSAGE_POSTFIX="have been synched"
 ERROR_MESSAGE="Rsync failed"
 ERROR_FLAG=false
 CURRENT_DIR=""
+
+#loopping through the directories
 for Dir in ${DIRS[@]}
 do
     CURRENT_DIR="$Dir"
@@ -32,10 +46,13 @@ do
 fi
 STATUS_MESSAGE+="$Dir "
 done
+
+#mail program should be installed and sendmail should be set as true using -m option argument
 if [[ "$ERROR_FLAG" = true && "$SENDMAIL" = true ]]; then
 MESSAGE="$STATUS_MESSAGE_PREFIX $STATUS_MESSAGE $STATUS_MESSAGE_POSTFIX"
 echo -e "$ERROR_MESSAGE on $CURRENT_DIR
 $MESSAGE" |mail -s "Rsync failed" -S from=rsync_error@Bahmni.failure.com $MAILING_ADDRESS_ON_FAILURE
 fi
+
 MESSAGE="$STATUS_MESSAGE_PREFIX $STATUS_MESSAGE $STATUS_MESSAGE_POSTFIX"
 echo $MESSAGE
