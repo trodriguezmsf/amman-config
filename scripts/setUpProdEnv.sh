5fe55221c7e62e1e2c992a370090f0e214db3b8f
Santhosh Bairam <bairamsa@thoughtworks.com>
2016-11-03 15:23:08 +0530

    Santhosh | refactored the scripts and removed db_dump url

--- scripts/setUpProdEnv.sh
+++ scripts/setUpProdEnv.sh
@@ -5,7 +5,28 @@ yum -y remove httpd
 cd /tmp
 wget http://ci-amman.mybahmni.org:8153/go/files/Bahmni_Build/134/download/1/buildOmod/omod/bahmnimsf-1.0-SNAPSHOT.omod
 yum -y install https://dl.bintray.com/bahmni/rpm/rpms/bahmni-installer-0.84-204.noarch.rpm # install bahmni latest version
-curl -L https://raw.githubusercontent.com/bahmni-msf/amman-config/master/dbdump/mysql_dump.sql > /etc/bahmni-installer/deployment-artifacts/openmrs_backup.sql
 cd /etc/bahmni-installer/deployment-artifacts/
 curl -L https://raw.githubusercontent.com/bahmni-msf/amman-config/master/inventories/setup.yml > /etc/bahmni-installer/setup.yml
 curl -L https://raw.githubusercontent.com/bahmni-msf/amman-config/master/inventories/prod > /etc/bahmni-installer/prod
+
+if [ ! -f "/etc/bahmni-installer/prod" ];
+then
+	echo "prod inventory file not found ";
+	exit;
+fi
+if [ ! -r /etc/bahmni-installer/setup.yml ];
+then
+	echo "setup.yml file not found ";
+	exit;
+fi
+if [ ! -e /tmp/bahmnimsf-1.0-SNAPSHOT.omod ];
+then
+	echo "bahmnimsf mode not found ,Kindly download it";
+	exit;
+fi
+cd /etc/bahmni-installer/deployment-artifacts
+rm -rf amman-config*
+rm -rf amman_config*
+wget https://github.com/bahmni-msf/amman-config/archive/master.zip
+unzip master.zip
+mv amman-config-master amman_config
