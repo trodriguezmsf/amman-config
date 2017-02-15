e13d856cb1e43e31c224e7f23728e5947debb1fb
Kaustav Chakraborty <kaustav.ron@gmail.com>
2017-02-15 16:24:47 +0530

    Kaustav | #190 | Fixed case conviction for floor and center in bed management. Added a new migration file

---
+++ openmrs/migrations/bedManagement/updateCasingForCenterAndFloor.sql
@@ -0,0 +1,3 @@
+UPDATE location SET name = "Rehabilitation Center", description = "Rehabilitation Center" WHERE name = "Rehabilitation center";
+
+UPDATE location SET name = "Ward (2nd floor)", description = "Ward (2nd floor)" WHERE name = "Ward (2nd Floor)";
