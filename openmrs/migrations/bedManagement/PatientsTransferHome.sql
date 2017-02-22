c025dd69c4b240edcea43d5e5001f0847e7f16da
shivakumar502 <shivakumarakula502@gmail.com>
2017-02-22 15:00:29 +0530

    Shiva Kumar | Changing sql file syntax in patienttransferhome

--- openmrs/migrations/bedManagement/PatientsTransferHome.sql
+++ openmrs/migrations/bedManagement/PatientsTransferHome.sql
@@ -7,13 +7,13 @@ DELETE FROM global_property where property = 'emrapi.sqlSearch.PatientsTransferH
           pi.identifier AS identifier,
           concat('', p.uuid) AS uuid,
           concat('', v.uuid) AS activeVisitUuid,
-          'Transfer home'                                               AS 'Status',
+            'Transfer to home'                                               AS 'Status',
           IF(va.value_reference = 'Admitted', 'true', 'false') as hasBeenAdmitted
         FROM visit v
         INNER JOIN person_name pn ON v.patient_id = pn.person_id and pn.voided is FALSE
         INNER JOIN patient_identifier pi ON v.patient_id = pi.patient_id and pi.voided is FALSE
         INNER JOIN patient_identifier_type pit on pi.identifier_type = pit.patient_identifier_type_id
-        INNER JOIN global_property gp on gp.property=emr.primaryIdentifierType and gp.property_value=pit.uuid
+        INNER JOIN global_property gp on gp.property='emr.primaryIdentifierType' and gp.property_value=pit.uuid
         INNER JOIN person p ON v.patient_id = p.person_id
         Inner Join (SELECT DISTINCT v.visit_id
           FROM encounter en
@@ -26,7 +26,7 @@ DELETE FROM global_property where property = 'emrapi.sqlSearch.PatientsTransferH
         INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = 'FULLY_SPECIFIED' AND cn.voided is FALSE
         JOIN location l on l.uuid=${visit_location_uuid} and v.location_id = l.location_id
         left outer join visit_attribute va on va.visit_id = v.visit_id and va.attribute_type_id =
-          (select visit_attribute_type_id from visit_attribute_type where name=Admission Status')
+          (select visit_attribute_type_id from visit_attribute_type where name='Admission Status')
         LEFT OUTER JOIN encounter e1 ON e1.visit_id = v.visit_id AND e1.encounter_type = (
           SELECT encounter_type_id
             FROM encounter_type
