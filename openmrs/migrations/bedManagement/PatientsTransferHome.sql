dae121fc1f5abfe8272bc1be7707ec61018b7244
shivakumar502 <shivakumarakula502@gmail.com>
2017-02-22 14:15:02 +0530

    Shiva kumar | Changing syntax in patienttransferhome

--- openmrs/migrations/bedManagement/PatientsTransferHome.sql
+++ openmrs/migrations/bedManagement/PatientsTransferHome.sql
@@ -5,10 +5,10 @@ DELETE FROM global_property where property = 'emrapi.sqlSearch.PatientsTransferH
 "SELECT DISTINCT
           concat(pn.given_name, ' ', pn.family_name) AS name,
           pi.identifier AS identifier,
-          concat("", p.uuid) AS uuid,
-          concat("", v.uuid) AS activeVisitUuid,
+          concat('', p.uuid) AS uuid,
+          concat('', v.uuid) AS activeVisitUuid,
           'Transfer home'                                               AS 'Status',
-          IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted
+          IF(va.value_reference = 'Admitted', 'true', 'false') as hasBeenAdmitted
         FROM visit v
         INNER JOIN person_name pn ON v.patient_id = pn.person_id and pn.voided is FALSE
         INNER JOIN patient_identifier pi ON v.patient_id = pi.patient_id and pi.voided is FALSE
@@ -20,15 +20,15 @@ DELETE FROM global_property where property = 'emrapi.sqlSearch.PatientsTransferH
           INNER JOIN visit v ON v.visit_id = en.visit_id AND en.encounter_type =
             (SELECT encounter_type_id
               FROM encounter_type
-            WHERE name = "ADMISSION")) v1 on v1.visit_id = v.visit_id
+            WHERE name = 'ADMISSION')) v1 on v1.visit_id = v.visit_id
         INNER JOIN encounter e ON v.visit_id = e.visit_id
         INNER JOIN obs o ON e.encounter_id = o.encounter_id
-        INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = "FULLY_SPECIFIED" AND cn.voided is FALSE
+        INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = 'FULLY_SPECIFIED' AND cn.voided is FALSE
         JOIN location l on l.uuid=${visit_location_uuid} and v.location_id = l.location_id
         left outer join visit_attribute va on va.visit_id = v.visit_id and va.attribute_type_id =
           (select visit_attribute_type_id from visit_attribute_type where name="Admission Status")
         LEFT OUTER JOIN encounter e1 ON e1.visit_id = v.visit_id AND e1.encounter_type = (
           SELECT encounter_type_id
             FROM encounter_type
-          WHERE name = "DISCHARGE") AND e1.voided is FALSE
+          WHERE name = 'DISCHARGE') AND e1.voided is FALSE
         WHERE v.date_stopped IS NULL AND v.voided = 0 AND o.voided = 0 AND cn.name = "Transfer Home" AND e1.encounter_id IS NULL;",'Patients Transfer to home',@uuid);
