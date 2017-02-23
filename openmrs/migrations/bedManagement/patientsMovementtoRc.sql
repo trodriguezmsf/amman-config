6558e78e3628307e5404010a110091b8c260020f
shivakumar502 <shivakumarakula502@gmail.com>
2017-02-23 12:11:35 +0530

    Shiva kumar | removing unwanted buttons  and changing conditions for ipd dispotion queues

--- openmrs/migrations/bedManagement/patientsMovementtoRc.sql
+++ openmrs/migrations/bedManagement/patientsMovementtoRc.sql
@@ -3,30 +3,32 @@ DELETE FROM global_property where property = 'emrapi.sqlSearch.PatientsMovementt
  INSERT INTO global_property (`property`, `property_value`, `description`, `uuid`)
  VALUES ('emrapi.sqlSearch.PatientsMovementtoRc',
 "SELECT DISTINCT
-  pi.identifier                                         AS identifier,
-  concat(pn.given_name, ' ', pn.family_name)            AS name,
-  p.gender                                              AS Gender,
-  floor(DATEDIFF(CURDATE(), p.birthdate) / 365)         AS 'Age',
-  MAX(DATE_FORMAT(o.obs_datetime,'%d %b %Y %h:%i %p'))  as 'Disposition Date',
-  'Movement to RC'                                               AS 'Status',
-  concat('', p.uuid)                                    AS uuid,
-  concat('', v.uuid)                                    AS activeVisitUuid
-FROM person p
-  JOIN person_name pn ON p.person_id = pn.person_id AND pn.voided IS FALSE AND p.voided IS FALSE
-  JOIN patient_identifier pi ON p.person_id = pi.patient_id AND pi.voided IS FALSE
-  JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id AND pit.retired IS FALSE
-  JOIN visit v ON p.person_id = v.patient_id AND v.voided IS FALSE
-  JOIN encounter e ON v.visit_id = e.visit_id AND e.voided IS FALSE
-  JOIN obs o ON e.encounter_id = o.encounter_id AND o.voided IS FALSE
-  JOIN concept c ON o.value_coded = c.concept_id AND c.retired IS FALSE
-  JOIN concept_name cn ON c.concept_id = cn.concept_id AND cn.voided IS FALSE
-  JOIN location l ON l.uuid = ${visit_location_uuid} AND v.location_id = l.location_id AND l.retired IS FALSE
-WHERE v.date_stopped IS NULL AND cn.name = 'Movement to RC' AND v.visit_id NOT IN (SELECT visit_id
-                                                                                  FROM encounter ie
-                                                                                    JOIN encounter_type iet
-                                                                                      ON iet.encounter_type_id =
-                                                                                         ie.encounter_type
-                                                                                         AND ie.voided IS FALSE
-                                                                                         AND iet.retired IS FALSE
-                                                                                  WHERE iet.name = 'ADMISSION')
-GROUP BY pi.identifier;",'Patients Movement to RC',@uuid);
+          concat(pn.given_name, ' ', pn.family_name) AS name,
+          pi.identifier AS identifier,
+          concat('', p.uuid) AS uuid,
+          concat('', v.uuid) AS activeVisitUuid,
+            'Transfer to home'                                               AS 'Status',
+          IF(va.value_reference = 'Admitted', 'true', 'false') as hasBeenAdmitted
+        FROM visit v
+        INNER JOIN person_name pn ON v.patient_id = pn.person_id and pn.voided is FALSE
+        INNER JOIN patient_identifier pi ON v.patient_id = pi.patient_id and pi.voided is FALSE
+        INNER JOIN patient_identifier_type pit on pi.identifier_type = pit.patient_identifier_type_id
+        INNER JOIN global_property gp on gp.property='emr.primaryIdentifierType' and gp.property_value=pit.uuid
+        INNER JOIN person p ON v.patient_id = p.person_id
+        Inner Join (SELECT DISTINCT v.visit_id
+          FROM encounter en
+          LEFT JOIN visit v ON v.visit_id = en.visit_id AND en.encounter_type =
+            (SELECT encounter_type_id
+              FROM encounter_type
+            WHERE name = 'ADMISSION')) v1 on v1.visit_id = v.visit_id
+        INNER JOIN encounter e ON v.visit_id = e.visit_id
+        INNER JOIN obs o ON e.encounter_id = o.encounter_id
+        INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = 'FULLY_SPECIFIED' AND cn.voided is FALSE
+        JOIN location l on l.uuid=${visit_location_uuid} and v.location_id = l.location_id
+        left outer join visit_attribute va on va.visit_id = v.visit_id and va.attribute_type_id =
+          (select visit_attribute_type_id from visit_attribute_type where name='Admission Status')
+        LEFT OUTER JOIN encounter e1 ON e1.visit_id = v.visit_id AND e1.encounter_type = (
+          SELECT encounter_type_id
+            FROM encounter_type
+          WHERE name = 'TRANSFER') AND e1.voided is FALSE
+        WHERE v.date_stopped IS NULL AND v.voided = 0 AND o.voided = 0 AND cn.name = 'Movement to RC' AND e1.encounter_id IS NULL;",'Patients Movement to RC',@uuid);
