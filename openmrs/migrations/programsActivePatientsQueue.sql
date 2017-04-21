9f61ac9b68e38afd4a7f7290d8d5e51554c0877a
shireesha <bongaralashireesha@gmail.com>
2017-04-21 14:47:03 +0530

    Shireesha | #000 | Showing only non voided person names on Programs queue

--- openmrs/migrations/programsActivePatientsQueue.sql
+++ openmrs/migrations/programsActivePatientsQueue.sql
@@ -5,8 +5,8 @@ SET property_value = "SELECT DISTINCT
     pi.identifier                                   AS identifier,
     concat('', p.uuid)                              AS uuid
 FROM patient_program ppr
-    INNER JOIN person_name pn ON ppr.patient_id = pn.person_id AND ppr.voided IS FALSE
-    INNER JOIN person p ON ppr.patient_id = p.person_id
-    INNER JOIN patient_identifier pi ON ppr.patient_id = pi.patient_id
+    INNER JOIN person_name pn ON ppr.patient_id = pn.person_id AND ppr.voided IS FALSE AND pn.voided IS FALSE
+    INNER JOIN person p ON ppr.patient_id = p.person_id AND p.voided IS FALSE
+    INNER JOIN patient_identifier pi ON ppr.patient_id = pi.patient_id AND pi.voided IS FALSE
 WHERE date_completed IS NULL;"
 WHERE property = "emrapi.sqlSearch.activePatients";
