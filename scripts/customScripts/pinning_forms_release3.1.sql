1430f1c8a3f29615f27f38d0fa43a8bbf069dace
Santhosh Bairam <bairamsa@thoughtworks.com>
2017-06-09 14:38:47 +0530

    script for pinning forms for release-3.0

---
+++ scripts/customScripts/pinning_forms_release3.1.sql
@@ -0,0 +1,262 @@
+SELECT user_id INTO @userid1 FROM users WHERE username ="osama_n";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid1, 'favouriteObsTemplates', “OPD Nursing Note###Nursing Needs - Ward###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+SELECT user_id INTO @userid2 FROM users WHERE username ="mohammad_b";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid2, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid3 FROM users WHERE username ="haya_aq";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid3, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid4 FROM users WHERE username ="firas_q";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid4, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid5 FROM users WHERE username ="salam_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid5, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid6 FROM users WHERE username ="yazan_h";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid6, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid7 FROM users WHERE username ="sami_ak";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid7, 'favouriteObsTemplates', "OPD Nursing Note###Nursing Needs - Ward###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid8 FROM users WHERE username ="eman_af";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid8, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid9 FROM users WHERE username ="yara_ae";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid9, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid10 FROM users WHERE username ="abeer_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid10, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid11 FROM users WHERE username ="qusay_o";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid11, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid12 FROM users WHERE username ="mohammad_ab";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid12, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid13 FROM users WHERE username ="ala_h";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid13, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid14 FROM users WHERE username ="mamon_q";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid14, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid15 FROM users WHERE username ="ayman_ao";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid15, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid16 FROM users WHERE username ="duaa_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid16, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid17 FROM users WHERE username ="safa_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid17, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid18 FROM users WHERE username ="mohammad_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid18, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid19 FROM users WHERE username ="suleiman_ak";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid19, 'favouriteObsTemplates', "OPD Nursing Note###Nursing Needs - Ward###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid20 FROM users WHERE username ="maamoun_j";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid20, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid21 FROM users WHERE username ="shahinaz_n";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid21, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid22 FROM users WHERE username ="tareq_f";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid22, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid23 FROM users WHERE username ="nisreen_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid23, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid24 FROM users WHERE username ="loay_m";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid24, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid25 FROM users WHERE username ="shadi_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid25, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid26 FROM users WHERE username ="baraa_ah";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid26, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid27 FROM users WHERE username ="deyaa_eb";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid27, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid28 FROM users WHERE username ="asma_z";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid28, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid29 FROM users WHERE username ="saleh_ar";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid29, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid30 FROM users WHERE username ="iyad_y";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid30, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid31 FROM users WHERE username ="samah_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid31, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+
+SELECT user_id INTO @userid32 FROM users WHERE username ="bdulrahman_af";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid32, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid33 FROM users WHERE username ="mohammed_r";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid33, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid34 FROM users WHERE username ="amro_ahm";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid34, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid35 FROM users WHERE username ="mohammad_am";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid35, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid36 FROM users WHERE username ="hamzah_m";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid36, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid37 FROM users WHERE username ="noor_n";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid37, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid38 FROM users WHERE username ="doaa_as";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid38, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid39 FROM users WHERE username ="ahmoud_ah";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid39, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid40 FROM users WHERE username ="harith_k";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid40, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid41 FROM users WHERE username ="khaled_ah";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid41, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid42 FROM users WHERE username ="osama_b";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid42, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid43 FROM users WHERE username ="waleed_bo";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid43, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid44 FROM users WHERE username ="majdi_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid44, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid45 FROM users WHERE username ="dina_aah";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid45, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid46 FROM users WHERE username ="khaldoon_ad";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid46, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid47 FROM users WHERE username ="abdullah_al";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid47, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid48 FROM users WHERE username ="hamzah_me";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid48, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid49 FROM users WHERE username ="yousef_aa";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid49, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid50 FROM users WHERE username ="abdallah_ad";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid50, 'favouriteObsTemplates', "OPD Nursing Note###Surgical Ward Admission Nursing Assessment###Ward Nursing Note###Post-Op Ward Monitoring");
+
+
+SELECT user_id INTO @userid51 FROM users WHERE username ="muntaha_m";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid51, 'favouriteObsTemplates', "Psychology initial assessment");
+
+
+
+SELECT user_id INTO @userid52 FROM users WHERE username ="qutaiba_am";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid52, 'favouriteObsTemplates', "Psychology initial assessment");
+
+
+
+SELECT user_id INTO @userid53 FROM users WHERE username ="nasser_ej";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid53, 'favouriteObsTemplates', "Psychology initial assessment");
+
+
+SELECT user_id INTO @userid54 FROM users WHERE username ="manar_h";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid54, 'favouriteObsTemplates', "Psychology initial assessment");
+
+
+SELECT user_id INTO @userid55 FROM users WHERE username ="helana_ak";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid55, 'favouriteObsTemplates', "Social and Medical History###Health Education");
+
+
+SELECT user_id INTO @userid56 FROM users WHERE username ="alaa_s";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid56, 'favouriteObsTemplates', "Social and Medical History###Health Education");
+
+
+SELECT user_id INTO @userid57 FROM users WHERE username ="naser_r";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid57, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid58 FROM users WHERE username ="rand_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid58, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid59 FROM users WHERE username ="rinad_h";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid59, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid60 FROM users WHERE username ="dima_h";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid60, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid61 FROM users WHERE username ="kamelah_a";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid61, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid62 FROM users WHERE username ="adham_by";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid62, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid63 FROM users WHERE username ="aziz_az";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid63, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
+
+
+SELECT user_id INTO @userid64 FROM users WHERE username ="zaid_se";
+INSERT INTO user_property(user_id, property, property_value) VALUES(@userid64, 'favouriteObsTemplates', "MD Initial Assessment###Complications Set###Physician Progress Note-Ward");
