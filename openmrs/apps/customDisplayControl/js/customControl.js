1813efa6207914505e910941c9874d82606dfe75
Santhosh Bairam <bairamsa@thoughtworks.com>
2017-09-07 21:04:16 +0530

    Santhosh, Pramida | #529 | Display control to show the patient movement history

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -227,4 +227,42 @@ angular.module('bahmni.common.displaycontrol.custom')
         link: link,
         template: '<ng-include src="contentUrl"/>'
     }
+}]).directive('patientMovementHistory', ['$http', '$stateParams', 'appService', 'spinner', function ($http, $stateParams, appService, spinner) {
+    var controller = function ($scope) {
+        $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/patientMovementHistory.html";
+        $scope.title = $scope.config.title;
+
+        var emitNoDataPresentEvent = function () {
+            return $scope.$emit("no-data-present-event");
+        };
+
+        var getResponseFromQuery = function () {
+            var params = {
+                patientUuid: $scope.patient.uuid,
+                visitUuid: $stateParams.visitUuid,
+                q: "bahmni.sqlGet.ipdPatientMovementHistory",
+                v: "full"
+            };
+            return $http.get('/openmrs/ws/rest/v1/bahmnicore/sql', {
+                method: "GET",
+                params: params,
+                withCredentials: true
+            });
+        };
+
+        spinner.forPromise(getResponseFromQuery().then(function (response) {
+            $scope.patientMovementHistory = response.data;
+            if ($scope.patientMovementHistory.length <= 0) {
+                emitNoDataPresentEvent();
+            } else {
+                $scope.headings = _.keys($scope.patientMovementHistory[0]);
+            }
+        }));
+    };
+
+    return {
+        restrict: 'E',
+        controller: controller,
+        template: '<ng-include src="contentUrl"/>'
+    }
 }]);
