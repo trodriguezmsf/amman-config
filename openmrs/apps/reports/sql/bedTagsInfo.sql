9bd39c9ed4addc1c426bfc27982b4be76522b9d6
Saikumar <saikv@thoughtworks.com>
2018-01-23 18:14:58 +0530

    Saikumar | #803 | Change date format in bed tags info sql

--- openmrs/apps/reports/sql/bedTagsInfo.sql
+++ openmrs/apps/reports/sql/bedTagsInfo.sql
@@ -1,8 +1,8 @@
 SELECT
   blm.bed_id,
   bt.name AS bed_tag_name,
-  btm.date_created AS tag_start_time,
-  coalesce(btm.date_voided,current_date) AS tag_end_time,
+  DATE_FORMAT(btm.date_created,'%c/%e/%y') AS tag_start_time,
+  DATE_FORMAT(btm.date_voided,'%c/%e/%y') AS tag_end_time,
   l.name AS location_name
 from bed_location_map blm
 INNER JOIN bed_tag_map btm ON btm.bed_id = blm.bed_id
