c0ad0b422bba960c04dc65a07badd15be3588777
Saikumar <saikv@thoughtworks.com>
2018-01-24 15:10:09 +0530

    Saikumar, Prasanna| #802 | Add date of presentation sql to indicators

---
+++ openmrs/apps/reports/sql/fstgDateOfPresentation.sql
@@ -0,0 +1,32 @@
+SELECT
+  dateOfPresentation.person_id,
+  dateOfPresentation.dateOfPresentation
+FROM
+  (SELECT
+     o.person_id,
+     GROUP_CONCAT(if(qcvn.concept_full_name = 'FSTG, Date of presentation at 1st stage', CAST(o.value_datetime AS DATE),
+                     NULL)) AS dateOfPresentation
+   FROM obs o
+     INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
+                                     qcvn.concept_full_name IN (
+                                       'FSTG, Date of presentation at 1st stage') AND qcvn.retired IS FALSE AND o.voided IS FALSE
+     LEFT JOIN concept_view acvn ON o.value_coded = acvn.concept_id AND acvn.retired IS FALSE
+   GROUP BY o.encounter_id
+  ) dateOfPresentation
+  INNER JOIN (
+               SELECT
+                 p.patient_id,
+                 cn.name,
+                 CAST(ps.start_date AS DATE)                    AS `startDate`,
+                 COALESCE(CAST(ps.end_date AS DATE), CURDATE()) AS `endDate`
+               FROM patient p
+                 INNER JOIN patient_program pp
+                   ON p.patient_id = pp.patient_id AND p.voided IS FALSE AND pp.voided IS FALSE
+                 INNER JOIN patient_state ps ON pp.patient_program_id = ps.patient_program_id AND ps.voided IS FALSE
+                 INNER JOIN program_workflow_state pws
+                   ON ps.state = pws.program_workflow_state_id AND ps.voided IS FALSE AND pws.retired IS FALSE
+                 INNER JOIN concept_name cn
+                   ON pws.concept_id = cn.concept_id AND cn.voided IS FALSE AND cn.concept_name_type = 'FULLY_SPECIFIED'
+               WHERE cn.name = 'Identification') identificationStateDetails
+    ON identificationStateDetails.patient_id = dateOfPresentation.person_id AND
+       dateOfPresentation BETWEEN startDate AND endDate
