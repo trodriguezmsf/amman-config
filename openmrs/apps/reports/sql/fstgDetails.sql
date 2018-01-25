00b7249f3b79f9639e9cb5e36ac85cb83c65405f
Saikumar <saikv@thoughtworks.com>
2018-01-25 18:45:43 +0530

    Saikumar, Prasanna | #802 | Add surgical daignosis data obs date time

--- openmrs/apps/reports/sql/fstgDetails.sql
+++ openmrs/apps/reports/sql/fstgDetails.sql
@@ -10,7 +10,8 @@ SELECT
   GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Type of anaesthesia administered', acvn.concept_full_name, null))                                 AS `typeOfAnaesthesia`,
   GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Anaesthesia start time', CAST(o.value_datetime AS DATE ), null))                         AS `anaesthesiaDateStarted`,
   GROUP_CONCAT(if(qcvn.concept_full_name = 'AIA, date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `aiaDateOfConsultation`,
-  GROUP_CONCAT(if(qcvn.concept_full_name = 'SAP, Date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `sapDateOfConsultation`
+  GROUP_CONCAT(if(qcvn.concept_full_name = 'SAP, Date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `sapDateOfConsultation`,
+  GROUP_CONCAT(if(qcvn.concept_full_name = 'Surgical Diagnosis Data', CAST(o.obs_datetime AS DATE ), null))                               AS `surgicalDiagnosisObsDate`
 FROM obs o
   INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
                                  qcvn.concept_full_name IN ('FSTG, Outcomes for 1st stage surgical validation',
@@ -23,7 +24,8 @@ FROM obs o
                                               'APN, Type of anaesthesia administered',
                                               'APN, Anaesthesia start time',
                                               'AIA, date of consultation',
-                                              'SAP, Date of consultation') AND qcvn.retired IS FALSE AND o.voided IS FALSE
+                                              'SAP, Date of consultation',
+                                              'Surgical Diagnosis Data') AND qcvn.retired IS FALSE AND o.voided IS FALSE
   LEFT JOIN concept_view acvn ON o.value_coded  = acvn.concept_id AND acvn.retired IS FALSE
   GROUP BY o.encounter_id
   ORDER BY o.person_id
