42a4e7dff8a67f4b292caf47857aa372ca91f954
Saikumar <saikv@thoughtworks.com>
2018-01-25 13:17:15 +0530

    Prasanna , Saikumar  | #802 | Add location to appointmentDetails sql

--- openmrs/apps/reports/sql/appointmentDetails.sql
+++ openmrs/apps/reports/sql/appointmentDetails.sql
@@ -1,5 +1,7 @@
-SELECT
-  patient_id,
-  CAST(start_date_time AS DATE ) AS `startDateTime`,
+select patient_id,
+  CAST(start_date_time AS DATE) AS startDate,
+  l.name AS location,
   status
-FROM patient_appointment;
+from patient_appointment pa
+  LEFT JOIN location l ON l.location_id = pa.location_id AND l.retired IS FALSE
+WHERE pa.voided IS FALSE
