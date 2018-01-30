c3747dd37d201679c342a15b922a585f99770291
Saikumar <saikv@thoughtworks.com>
2018-01-30 12:53:29 +0530

    Saikumar | #802 | Update anaesthesiaFilled sql to separate different types of anaesthesias

--- openmrs/apps/reports/sql/patientsWithAnaesthesiaFilled.sql
+++ openmrs/apps/reports/sql/patientsWithAnaesthesiaFilled.sql
@@ -1,18 +1,30 @@
 SELECT
-  o.person_id,
-  GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Type of anaesthesia administered', acvn.concept_full_name, null))                                 AS `typeOfAnaesthesia`,
-  GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Anaesthesia start time', CAST(o.value_datetime AS DATE ), null))                         AS `anaesthesiaDateStarted`,
-  CAST(dateOfArrival.`value` AS DATE)                                                                                                                             AS `dateOfArrival`
-FROM obs o
-  INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
-                                 qcvn.concept_full_name IN (
-                                              'APN, Type of anaesthesia administered',
-                                              'APN, Anaesthesia start time') AND qcvn.retired IS FALSE AND o.voided IS FALSE
-  LEFT JOIN concept_view acvn ON o.value_coded  = acvn.concept_id AND acvn.retired IS FALSE
-  LEFT JOIN (SELECT
-                pa.person_id,
-                DATE_FORMAT(pa.value,'%c/%e/%y') as `value`
-    FROM person_attribute pa
-  INNER JOIN person_attribute_type pat ON pa.person_attribute_type_id = pat.person_attribute_type_id AND pat.name = 'dateOfArrival'
-  ) dateOfArrival ON o.person_id = dateOfArrival.person_id
-GROUP BY o.encounter_id
+  patient_id,
+  typeOfAnaesthesia,
+  IF((typeOfAnaesthesia LIKE '%GA%' AND typeOfAnaesthesia NOT LIKE '%R%' AND
+      typeOfAnaesthesia NOT LIKE '%LOA%'
+      AND typeOfAnaesthesia NOT LIKE '%SED%' AND typeOfAnaesthesia NOT LIKE '%INF%'),'Yes', 'No')           AS `generalAnaesthesia`,
+  IF((typeOfAnaesthesia LIKE '%R%' AND typeOfAnaesthesia NOT LIKE '%GA%' AND
+      typeOfAnaesthesia NOT LIKE '%LOA%'
+      AND typeOfAnaesthesia NOT LIKE '%SED%' AND typeOfAnaesthesia NOT LIKE '%INF%'), 'Yes', 'No')          AS `regionalAnaesthesia`,
+  IF(((typeOfAnaesthesia LIKE '%SED%' OR typeOfAnaesthesia LIKE '%LOA%' OR typeOfAnaesthesia LIKE '%INF%')
+      AND typeOfAnaesthesia NOT LIKE '%GA%' AND typeOfAnaesthesia NOT LIKE '%R%'), 'Yes', 'No')             AS `LSIAnaesthesia`,
+  IF((typeOfAnaesthesia LIKE '%GA%' AND (typeOfAnaesthesia LIKE '%R%' OR typeOfAnaesthesia LIKE '%LOA%' OR
+                                        typeOfAnaesthesia LIKE '%SED%' OR typeOfAnaesthesia LIKE '%INF%')) OR
+     (typeOfAnaesthesia LIKE '%R%' AND (typeOfAnaesthesia LIKE '%LOA%' OR typeOfAnaesthesia LIKE '%SED%' OR
+                                        typeOfAnaesthesia LIKE '%INF%')), 'Yes', 'No')                      AS `mixedAnaesthesia`,
+  CAST(anaesthesiaDateStarted AS DATE ) AS anaesthesiaDateStarted
+FROM
+  (SELECT
+     o.person_id                                                                                                      AS patient_id,
+     GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Type of anaesthesia administered', acvn.concept_full_name,
+                     NULL))                                                                                           AS `typeOfAnaesthesia`,
+     GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Anaesthesia start time', CAST(o.value_datetime AS DATE),
+                     NULL))                                                                                           AS `anaesthesiaDateStarted`
+   FROM obs o
+     INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
+                                     qcvn.concept_full_name IN (
+                                       'APN, Type of anaesthesia administered',
+                                       'APN, Anaesthesia start time') AND qcvn.retired IS FALSE AND o.voided IS FALSE
+     LEFT JOIN concept_view acvn ON o.value_coded = acvn.concept_id AND acvn.retired IS FALSE
+   GROUP BY o.encounter_id) typeOfAnaestesiaInfo;
