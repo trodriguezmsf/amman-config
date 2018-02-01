e82601ec77b7241102ed1e2fd7864078c08e2a23
Saikumar <saikv@thoughtworks.com>
2018-02-01 19:47:55 +0530

    Prasanna , Saikumar | #802 | Modify all bed occupancy sqls

--- openmrs/apps/reports/sql/uniqueBedDetailsRC.sql
+++ openmrs/apps/reports/sql/uniqueBedDetailsRC.sql
@@ -1,26 +1,25 @@
 SELECT
-    CAST(bpam.date_created AS DATE )    AS `admissionDate`,
-    bpam.patient_id,
-    b.bed_id,
-    b.bed_number,
-    l.name               AS `location`,
-    CAST(dischargeDetails.bed_discharge_date AS DATE ) AS `Discharge Time`
-FROM bed b
-    INNER JOIN bed_location_map blm ON blm.bed_id = b.bed_id AND b.voided IS FALSE
-    INNER JOIN bed_patient_assignment_map bpam ON b.bed_id = bpam.bed_id AND bpam.voided IS FALSE
-    INNER JOIN
-    (
-        SELECT
-            bpam.patient_id,
-            MAX(bpam.date_stopped) AS `bed_discharge_date`,
-            dischargeTimes.date_created
-        FROM bed_patient_assignment_map bpam
-            INNER JOIN (
-                           SELECT e.patient_id, e.date_created FROM encounter e
-                               INNER JOIN encounter_type et ON e.encounter_type = et.encounter_type_id and et.name = 'DISCHARGE'
-                       ) dischargeTimes ON bpam.patient_id = dischargeTimes.patient_id AND bpam.date_stopped <= dischargeTimes.date_created
-        GROUP BY bpam.patient_id,dischargeTimes.date_created
-    ) dischargeDetails ON bpam.patient_id = dischargeDetails.patient_id AND bpam.date_stopped = dischargeDetails.bed_discharge_date
-    INNER JOIN location l ON blm.location_id = l.location_id AND l.retired IS FALSE
-    WHERE ((l.name IN ('Rehabilitation Center (4th floor)', 'Rehabilitation Center (5th floor)')) AND (YEAR(bpam.date_Created) = YEAR('#startDate#')))
-ORDER BY bpam.patient_id;
+  wardOccupency.date_started,
+  wardOccupency.patient_id,
+  wardOccupency.bed_id,
+  '' as bedNumber,
+  '' as location,
+  wardOccupency.date_stopped
+  FROM
+(SELECT
+  patient_id,
+  bed_id,
+  cast(date_started AS DATE ) AS date_started,
+  cast(IF(date_stopped IS NULL, '2017-12-31', date_stopped) AS DATE ) AS date_stopped
+ FROM bed_patient_assignment_map bpam
+WHERE bed_id IN (SELECT bed_id
+                 FROM bed_location_map blm
+                 WHERE location_id IN (
+                   SELECT
+                     l.location_id
+                   FROM location l WHERE l.name IN (
+                     'Rehabilitation Center (4th floor)',
+                     'Rehabilitation Center (5th floor)'
+                   )
+                 ) AND bpam.voided IS FALSE ) AND bpam.voided IS FALSE AND year(date_started)=YEAR('#startDate#') AND year(date_stopped)=YEAR('#startDate#')
+) wardOccupency
