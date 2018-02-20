57c08c9b2db565a3923a9896b2c672448fec7fe1
Saikumar <saikv@thoughtworks.com>
2018-02-20 15:48:20 +0530

    Saikumar | #802 | Remove multiple comma separated dates in single row

--- openmrs/apps/reports/sql/fstgDetails.sql
+++ openmrs/apps/reports/sql/fstgDetails.sql
@@ -2,18 +2,18 @@ SELECT *
 FROM
   (SELECT
      o.person_id,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'FSTG, Date received', CAST(o.value_datetime AS DATE ), null))                                 AS `dateOfFileRecieved`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'FSTG, Date of presentation at 1st stage', CAST(o.value_datetime AS DATE ), null))             AS `dateOfPresentation`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'FSTG, Outcomes for 1st stage surgical validation', acvn.concept_full_name, null))                      AS `outComeOfFSTG`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'ONN, Was the dressing changed', acvn.concept_full_name, null))                                         AS `wasTheDressingChanged`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'FSTG, Date received', CAST(o.value_datetime AS DATE ), null))                                 AS `dateOfFileRecieved`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'FSTG, Date of presentation at 1st stage', CAST(o.value_datetime AS DATE ), null))             AS `dateOfPresentation`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'FSTG, Outcomes for 1st stage surgical validation', acvn.concept_full_name, null))                      AS `outComeOfFSTG`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'ONN, Was the dressing changed', acvn.concept_full_name, null))                                         AS `wasTheDressingChanged`,
      NULL                                                                                                                                    AS `xRayUploaded`,
      NULL                                                                                                                                    AS `xRayUploadedDate`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'ONN, Date recorded', CAST(o.value_datetime AS DATE ), null))                                  AS `dateRecorderedForONN`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Type of anaesthesia administered', acvn.concept_full_name, null))                                 AS `typeOfAnaesthesia`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'APN, Anaesthesia start time', CAST(o.value_datetime AS DATE ), null))                         AS `anaesthesiaDateStarted`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'AIA, date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `aiaDateOfConsultation`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'SAP, Date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `sapDateOfConsultation`,
-     GROUP_CONCAT(if(qcvn.concept_full_name = 'Surgical Diagnosis Data', CAST(o.obs_datetime AS DATE ), null))                               AS `surgicalDiagnosisObsDate`
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'ONN, Date recorded', CAST(o.value_datetime AS DATE ), null))                                  AS `dateRecorderedForONN`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'APN, Type of anaesthesia administered', acvn.concept_full_name, null))                                 AS `typeOfAnaesthesia`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'APN, Anaesthesia start time', CAST(o.value_datetime AS DATE ), null))                         AS `anaesthesiaDateStarted`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'AIA, date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `aiaDateOfConsultation`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'SAP, Date of consultation', CAST(o.value_datetime AS DATE ), null))                           AS `sapDateOfConsultation`,
+     GROUP_CONCAT(DISTINCT if(qcvn.concept_full_name = 'Surgical Diagnosis Data', CAST(o.obs_datetime AS DATE ), null))                               AS `surgicalDiagnosisObsDate`
    FROM obs o
      INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
                                      qcvn.concept_full_name IN ('FSTG, Outcomes for 1st stage surgical validation',
@@ -38,8 +38,8 @@ FROM
       NULL                                                                                               AS `dateOfPresentation`,
       NULL                                                                                               AS `outComeOfFSTG`,
       NULL                                                                                               AS `wasTheDressingChanged`,
-      GROUP_CONCAT(IF(qcvn.concept_full_name = 'Documents, Image Upload', 'Yes', NULL))                  AS `xRayUploaded`,
-      GROUP_CONCAT(IF(qcvn.concept_full_name = 'Documents, Date', CAST(o.value_datetime AS DATE), NULL)) AS `xRayUploadedDate`,
+      GROUP_CONCAT(DISTINCT IF(qcvn.concept_full_name = 'Documents, Image Upload', 'Yes', NULL))                  AS `xRayUploaded`,
+      GROUP_CONCAT(DISTINCT IF(qcvn.concept_full_name = 'Documents, Date', CAST(o.value_datetime AS DATE), NULL)) AS `xRayUploadedDate`,
       NULL                                                                                               AS `dateRecorderedForONN`,
       NULL                                                                                               AS `typeOfAnaesthesia`,
       NULL                                                                                               AS `anaesthesiaDateStarted`,
