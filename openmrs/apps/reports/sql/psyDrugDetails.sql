3987c7cf188749336eecf6ff59bcac92c363e9ed
Saikumar <saikv@thoughtworks.com>
2018-02-21 19:14:57 +0530

    Saikumar | #838 | Add provider name condition to psyDrug sql

--- openmrs/apps/reports/sql/psyDrugDetails.sql
+++ openmrs/apps/reports/sql/psyDrugDetails.sql
@@ -1,12 +1,14 @@
 SELECT e.patient_id,
-  name,
+  d.name,
   CAST(o.date_activated AS DATE) AS psyDrugStartDate,
   CAST(coalesce(o.date_stopped, o.auto_expire_date) AS DATE)  AS psyDrugEndDate
 FROM drug d
-  INNER JOIN orders o ON o.concept_id = d.concept_id AND order_action = 'NEW'
+  INNER JOIN orders o ON o.concept_id = d.concept_id AND order_action = 'NEW' AND o.voided IS FALSE AND d.retired IS FALSE
   INNER JOIN drug_order od ON od.order_id = o.order_id
-  INNER JOIN encounter e ON e.encounter_id = o.encounter_id
-where name IN (
+  INNER JOIN encounter e ON e.encounter_id = o.encounter_id AND e.voided IS FALSE
+  INNER JOIN provider p ON e.creator = p.provider_id AND p.retired IS FALSE
+  INNER JOIN person_name pn ON p.person_id = pn.person_id AND pn.voided IS FALSE
+WHERE concat(pn.given_name, ' ', pn.family_name) = 'Super Man' AND d.name IN (
   'AMITRIPTYLINE hydrochloride 25 mg',
   'BROMAZEPAM 3mg',
   'CARBAMAZEPINE 200 mg',
