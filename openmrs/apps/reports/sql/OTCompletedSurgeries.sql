207709cfd7a7cce57ad7539f1bc54d6612b453af
Saikumar <saikv@thoughtworks.com>
2018-02-26 11:43:55 +0530

    Saikumar | #802 | Add alias to column in sql

--- openmrs/apps/reports/sql/OTCompletedSurgeries.sql
+++ openmrs/apps/reports/sql/OTCompletedSurgeries.sql
@@ -1,5 +1,5 @@
 SELECT patient_id,
-  CAST(actual_start_datetime AS DATE ),
+  CAST(actual_start_datetime AS DATE ) AS surgeryStartDate,
   status
 FROM surgical_appointment
 WHERE status = 'COMPLETED' AND YEAR(actual_start_datetime) = YEAR('#startDate#')
