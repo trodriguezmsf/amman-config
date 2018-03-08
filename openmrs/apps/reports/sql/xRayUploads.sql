67449289b06d943876965dc09f26c11cf764bf62
Mahesh <maheshva@thoughtworks.com>
2018-03-08 18:00:21 +0530

    Maharjun, Mahesh | #808 | Xray Uploads

--- openmrs/apps/reports/sql/xRayUploads.sql
+++ openmrs/apps/reports/sql/xRayUploads.sql
@@ -1,9 +1,12 @@
 SELECT
   o.person_id,
-  CAST(o.obs_datetime AS DATE) AS xRayUploadedDate
-FROM obs o
-  INNER JOIN concept_view qcvn ON qcvn.concept_id = o.concept_id AND
-                                  qcvn.concept_full_name IN (
-                                    'Documents, Image Upload'
-                                  ) AND qcvn.retired IS FALSE AND o.voided IS FALSE
-ORDER BY o.person_id;
+  CAST(dateObs.value_datetime as DATE) AS Date
+from obs o
+  INNER JOIN concept_view imageCV ON imageCV.concept_id = o.concept_id AND o.voided IS FALSE AND imageCV.retired IS FALSE AND imageCV.concept_full_name = 'Documents, Image Upload'
+  INNER JOIN obs imageObs ON o.obs_id = imageObs.obs_group_id AND imageObs.voided IS FALSE
+  INNER JOIN concept_view imageConcept ON imageObs.concept_id = imageConcept.concept_id AND imageConcept.retired IS FALSE AND
+imageConcept.concept_full_name = 'Image'
+  INNER JOIN (
+               SELECT o.value_datetime, o.obs_group_id from obs o
+                 INNER JOIN concept_view cv ON cv.concept_id = o.concept_id AND cv.retired IS FALSE AND o.voided IS FALSE AND cv.concept_full_name = 'Documents, Date'
+             ) dateObs ON dateObs.obs_group_id = o.obs_group_id;
