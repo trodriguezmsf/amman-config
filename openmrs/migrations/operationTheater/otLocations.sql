2eaecc30c28018947105fb8232a9e4a773396e53
Saikumar <saikv@thoughtworks.com>
2018-05-13 14:46:17 +0530

    Saikumar , Maharjun | #000 | Remove insert ot location tag statement as this task would have already by migration from OT omod

--- openmrs/migrations/operationTheater/otLocations.sql
+++ openmrs/migrations/operationTheater/otLocations.sql
@@ -1,5 +1,3 @@
-INSERT INTO location_tag(name, description, creator, date_created, retired, uuid) VALUES ('Operation Theater', 'Operation Theater', 1, now(), 0, uuid());
-
 INSERT INTO location (name, description, retired, uuid, date_created, creator) VALUES ('OT 1', 'Operation Theater 1', FALSE, uuid(), NOW(), 1);
 INSERT INTO location (name, description, retired, uuid, date_created, creator) VALUES ('OT 2', 'Operation Theater 2', FALSE, uuid(), NOW(), 1);
 INSERT INTO location (name, description, retired, uuid, date_created, creator) VALUES ('OT 3', 'Operation Theater 3', FALSE, uuid(), NOW(), 1);
