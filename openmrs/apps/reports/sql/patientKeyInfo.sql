eaacfbe220881064ad8a290e40f37d14a477e77b
Vinisha Donthula <vinishad@thoughtworks.com>
2020-02-03 15:39:04 +0530

    Siva, Vinisha | MOBN-1015 | Update id's with names in Patient key information report

--- openmrs/apps/reports/sql/patientKeyInfo.sql
+++ openmrs/apps/reports/sql/patientKeyInfo.sql
@@ -28,6 +28,7 @@ select distinct patient_identifier.identifier                                   
 
 from patient_identifier
        join person_name on person_name.person_id = patient_identifier.patient_id
+-- Start of Label 1: TO GET REQUIRED PERSON ATTRIBUTES FILLED FOR A PERSON
        join (select person_id,
                     group_concat(first_name)                             as first_name,
                     group_concat(last_name)                              as last_name,
@@ -55,49 +56,74 @@ from patient_identifier
                     group_concat(caretaker_viber)                        as caretaker_viber,
                     group_concat(caretaker_facebook)                     as caretaker_facebook
 
-             from ((select person_attribute.person_id, person_attribute.value,
-                           case when person_attribute_type_id = 8 then value end                         as first_name,
-                           case when person_attribute_type_id = 9 then value end                         as last_name,
-                           case when person_attribute_type_id = 74 then value end                        as is_caretaker_required,
-                           case
-                             when person_attribute_type_id = 52 then (select concept_name.name
-                                                                      from concept_name
-                                                                      where concept_name.concept_id = value
-                                                                        and concept_name.voided = 0) end as caretaker_gender,
-                           case when person_attribute_type_id = 67 then value end                        as caretaker_name_english,
-                           case when person_attribute_type_id = 58 then value end                        as caretaker_name_arabic,
-                           case
-                             when person_attribute_type_id = 76 then (select concept_name.name
-                                                                      from concept_name
-                                                                      where concept_name.concept_id = value
-                                                                        and concept_name.voided = 0) end as status_of_id_documents,
-                           case when person_attribute_type_id = 31 then value end                        as patient_id_document_number,
-                           case when person_attribute_type_id = 26 then value end                        as caretaker_id_document_number,
-                           case when person_attribute_type_id = 39 then value end                        as patient_id_expiry_date,
-                           case when person_attribute_type_id = 30 then value end                        as caretaker_id_expiry_date,
-                           case when person_attribute_type_id = 18 then value end                        as patient_phone_number_1,
-                           case when person_attribute_type_id = 14 then value end                        as patient_phone_number_2,
-                           case when person_attribute_type_id = 75 then value end                        as patient_phone_number_3,
-                           case when person_attribute_type_id = 17 then value end                        as patient_email_address,
-                           case when person_attribute_type_id = 16 then value end                        as patient_whatsapp,
-                           case when person_attribute_type_id = 13 then value end                        as patient_viber,
-                           case when person_attribute_type_id = 15 then value end                        as patient_facebook,
-                           case when person_attribute_type_id = 57 then value end                        as caretaker_phone_number_1,
-                           case when person_attribute_type_id = 59 then value end                        as caretaker_phone_number_2,
-                           case when person_attribute_type_id = 70 then value end                        as caretaker_whatsapp,
-                           case when person_attribute_type_id = 48 then value end                        as caretaker_viber,
-                           case when person_attribute_type_id = 54 then value end                        as caretaker_facebook
+             from (
+             (select person_attribute.person_id,
+       person_attribute.value,
+       case when person_attribute_type.name = 'givenNameLocal' then value end                                     as first_name,
+       case when person_attribute_type.name = 'familyNameLocal' then value end                                    as last_name,
+       case
+         when person_attribute_type.name = 'isCareTakerRequired'
+                 then value end                                                                                   as is_caretaker_required,
+       case
+         when person_attribute_type.name = 'caretakerGender' then (select concept_name.name
+                                                                   from concept_name
+                                                                   where concept_name.concept_id = value
+                                                                     and concept_name.voided = 0) end             as caretaker_gender,
+       case
+         when person_attribute_type.name = 'caretakerNameEnglish'
+                 then value end                                                                                   as caretaker_name_english,
+       case
+         when person_attribute_type.name = 'caretakerNameArabic'
+                 then value end                                                                                   as caretaker_name_arabic,
+       case
+         when person_attribute_type.name = 'statusofOfficialIDdocuments' then (select concept_name.name
+                                                                               from concept_name
+                                                                               where concept_name.concept_id = value
+                                                                                 and concept_name.voided = 0) end as status_of_id_documents,
+       case when person_attribute_type.name = 'id1DocNumber' then value end                                       as patient_id_document_number,
+       case when person_attribute_type.name = 'id3DocNumber' then value end                                       as caretaker_id_document_number,
+       case when person_attribute_type.name = 'id1DateOfExpiry' then value end                                    as patient_id_expiry_date,
+       case when person_attribute_type.name = 'id3DateOfExpiry' then value end                                    as caretaker_id_expiry_date,
+       case when person_attribute_type.name = 'phoneNumber1' then value end                                       as patient_phone_number_1,
+       case when person_attribute_type.name = 'phoneNumber2' then value end                                       as patient_phone_number_2,
+       case when person_attribute_type.name = 'phoneNumber3' then value end                                       as patient_phone_number_3,
+       case when person_attribute_type.name = 'emailAddress' then value end                                       as patient_email_address,
+       case when person_attribute_type.name = 'whatsapp' then value end                                           as patient_whatsapp,
+       case when person_attribute_type.name = 'viber' then value end                                              as patient_viber,
+       case when person_attribute_type.name = 'facebook' then value end                                           as patient_facebook,
+       case
+         when person_attribute_type.name = 'caretakerPhoneNumber1'
+                 then value end                                                                                   as caretaker_phone_number_1,
+       case
+         when person_attribute_type.name = 'caretakerPhoneNumber2'
+                 then value end                                                                                   as caretaker_phone_number_2,
+       case
+         when person_attribute_type.name = 'caretakerWhatsapp'
+                 then value end                                                                                   as caretaker_whatsapp,
+       case when person_attribute_type.name = 'caretakerViber' then value end                                     as caretaker_viber,
+       case
+         when person_attribute_type.name = 'caretakerFacebook'
+                 then value end                                                                                   as caretaker_facebook
 
-                    from person_attribute where voided = 0) as x
+       from person_attribute
+       join person_attribute_type
+         on person_attribute.person_attribute_type_id = person_attribute_type.person_attribute_type_id where voided=0) as person_attributes_with_name
                  )
 
              group by person_id) as pivoted_person_attributes
          on pivoted_person_attributes.person_id = patient_identifier.patient_id
+-- End of Label 1
        join person_address on person_address.person_id = patient_identifier.patient_id
+-- Start of Label 2: TO GET DATE WHEN THE PASSPORT GOT UPLOADED INTO THE SYSTEM
        left join (select obs.person_id, max(obs.date_created) as date_of_password_upload
              from obs
-             where concept_id = 142
+             where concept_id = (select concept_id
+                                from concept_name
+                                where name = 'Passport'
+                                  and concept_name_type = 'FULLY_SPECIFIED'
+                                  and voided = 0)
                and voided = 0 group by person_id) as obs_data on obs_data.person_id = patient_identifier.patient_id
+-- End of Label 2
 order by patient_identifier.date_created desc;
 
 
