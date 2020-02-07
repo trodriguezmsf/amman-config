ea7f7a1e76efb0a3c4258cca4eeb726b7c81156c
Michaël Bontyes <michaelbontyes@gmail.com>
2020-02-07 10:49:02 -0500

    MOBN-845 | Updating procedure to update concepts

--- openmrs/migrations/utils/UpdateConcept.sql
+++ openmrs/migrations/utils/UpdateConcept.sql
@@ -6,6 +6,7 @@ CREATE PROCEDURE update_concept (concept_fully_specified_name VARCHAR(255),
                              updated_concept_form_text VARCHAR(255),
                              updated_concept_datatype_id INT(11),
                              updated_concept_class_id INT(11),
+                             updated_concept_units VARCHAR(50),
                              updated_concept_is_set INT(11))
 
 BEGIN
@@ -14,11 +15,9 @@ BEGIN
 
  # Check if concept name exists
  SELECT COUNT(DISTINCT concept_id) INTO @concept_count FROM concept_name WHERE name = concept_fully_specified_name AND concept_name_type='FULLY_SPECIFIED' AND voided = 0 AND locale = concept_locale;
-
  IF @concept_count = 0 THEN
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = 'Concept Name Not Found';
-
  ELSE
 
     # Retrieve concept_id from concept concept_fully_specified_name
@@ -43,9 +42,15 @@ BEGIN
 
     # Update concept_description
     IF updated_concept_description IS NOT NULL THEN
-        UPDATE concept
-        SET description = updated_concept_description
-        WHERE concept_id = concept_id_proc AND retired = 0;
+        SELECT COUNT(DISTINCT concept_id) INTO @concept_count FROM concept_description WHERE concept_id = concept_id_proc;
+         IF @concept_count = 0 THEN
+            INSERT INTO openmrs.concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid) VALUES
+            (concept_id_proc,updated_concept_description,concept_locale,user_id_proc,now(),user_id_proc,now(),uuid());
+         ELSE
+            UPDATE concept_description
+            SET description = updated_concept_description
+            WHERE concept_id = concept_id_proc AND locale = concept_locale;
+         END IF;
     END IF;
 
     # Update concept_form_text
@@ -67,6 +72,19 @@ BEGIN
         UPDATE concept
         SET class_id = updated_concept_class_id
         WHERE concept_id = concept_id_proc AND retired = 0;
+    END IF;
+
+    # Update concept_units
+    IF updated_concept_units IS NOT NULL THEN
+        SELECT COUNT(DISTINCT concept_id) INTO @concept_count FROM concept_numeric WHERE concept_id = concept_id_proc;
+         IF @concept_count = 0 THEN
+            INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision) VALUES
+            (concept_id_proc,NULL,NULL,NULL,NULL,NULL,NULL,updated_concept_units,1,NULL);
+         ELSE
+            UPDATE concept_numeric
+            SET units = updated_concept_units
+            WHERE concept_id = concept_id_proc;
+         END IF;
     END IF;
 
     # Update concept_is_set
