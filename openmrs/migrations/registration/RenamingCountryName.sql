1fe308f582672333faa05659c964b0ae3195ff31
Michaël Bontyes <michaelbontyes@gmail.com>
2020-03-16 16:22:10 -0400

    MOBN-831 | Revision (Country column only)

--- openmrs/migrations/registration/RenamingCountryName.sql
+++ openmrs/migrations/registration/RenamingCountryName.sql
@@ -5,14 +5,9 @@ SET @newValue := "Palestine";
 # Renaming Address Hierarchy Entry Country value
 UPDATE address_hierarchy_entry
 SET name = @newValue
-WHERE name = @currentValue;
+WHERE name = @currentValue AND level_id = 1;
 
 # Renaming Person Address3 value
 UPDATE person_address
 SET address3 = @newValue
 WHERE address3 = @currentValue;
-
-# Renaming Person Address6 value
-UPDATE person_address
-SET address6 = @newValue
-WHERE address6 = @currentValue;
