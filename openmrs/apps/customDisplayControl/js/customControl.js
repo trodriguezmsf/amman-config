5c18951f38aec97176abed6916238cb0d7c2e4a5
Pritam Das <matirp@Matirps-MacBook.local>
2020-03-20 14:04:04 +0530

    MOBN-849 | Add new sites of wound to custom display control and reorder wound description answers

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -1317,29 +1317,14 @@ angular.module('bahmni.common.displaycontrol.custom')
             });
         };
 
-        const getPhysicalExaminationData = function (encounterData) {
-            var concepts = [];
-            _.each(encounterData.groupMembers, function (groupMember) {
-                if(groupMember.groupMembers.length > 0) {
-                    console.log("Sub Group Members case");
-                    getPhysicalExaminationData(groupMember);
-                }
-                var conceptName = {name: groupMember.conceptNameToDisplay, answer: groupMember.valueAsString}
-                concepts.push(conceptName)
-            });
-            return concepts;
 
-        };
-        const getPhysicalExaminationData1 = function (encounterData, concepts = []) {
-            //var concepts = [];
+        const getPhysicalExaminationData = function (encounterData, concepts = []) {
             _.each(encounterData.groupMembers, function (groupMember) {
                 if(groupMember.groupMembers.length > 0) {
-                    console.log("Sub Group Members case");
                     var values = getMultiSelectValues(groupMember);
                     concepts.push({name: groupMember.conceptNameToDisplay, answer: values});
                     return;
                 }
-
                 var conceptName = {name: groupMember.conceptNameToDisplay, answer: groupMember.valueAsString}
                 concepts.push(conceptName)
             });
@@ -1348,28 +1333,26 @@ angular.module('bahmni.common.displaycontrol.custom')
         };
 
         const getMultiSelectValues = function (groupMember)  {
-            let values = '';
+            let values = [];
             _.each(groupMember.groupMembers, function(multiSelectObs) {
-                values += ' ' + multiSelectObs.valueAsString;
-            })
-            return values;
+                if(multiSelectObs.conceptNameToDisplay == "Site of wound"){
+                    values.push(multiSelectObs.valueAsString);
+
+                }
+            });
+            return values.join(', ');
         }
 
 
         const getDisplayableEncounter = function (records, methods) {
-            console.log("Values ", JSON.parse(JSON.stringify(records)));
             var allValues = [];
             _.forEach(methods, function (method) {
                 var encounterValue = method(records);
                 if (!_.isEmpty(encounterValue)) {
-                    console.log("encounterValue ", JSON.parse(JSON.stringify(encounterValue)));
                     var value = getPhysicalExaminationData(encounterValue);
-                    var value = getPhysicalExaminationData1(encounterValue);
                     allValues.push({date: encounterValue.encounterDateTime, value: value})
                 }
             });
-            console.log("allValues ", JSON.parse(JSON.stringify(allValues)));
-
             return allValues;
         };
 
